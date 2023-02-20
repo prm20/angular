29e247f3292fbb871565cd2021c01cfd7cd1c630
preston <pmatson12@outlook.com>
2023-02-20 00:06:39 -0800

    property binding

--- udemy course/my-first-app/src/app/servers/servers.component.ts
+++ udemy course/my-first-app/src/app/servers/servers.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    },2000)
+
+  }
+
+  ngOnInit(){
+
+  }
+
 }
